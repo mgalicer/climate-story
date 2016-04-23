e5f49208c3e877578d1ec6815f03f4c37fbf01d9
Maggie Love <maggie.love1991@gmail.com>
2016-04-23 19:50:50 -0400

    working on get current date, getTimeMachine functions

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,4 @@
-'use strict';
+// 'use strict';
 
 $( document ).ready(function() {
      navigator.geolocation.getCurrentPosition(function(position) {
@@ -12,9 +12,22 @@ var wuKey;
 
 var forecastKey;
 
+
 //Figure out how to pull the current date
-//Then convert the date to YYYYMMDD format.
-var currentDate;
+//Then convert the date to UNIX time to fit Forecast.io API call format
+var currentDate = Date.now();
+console.log('date: ' + currentDate);
+var lat = pos[0];
+var long = pos[1];
+
+function getTimeMachine(){
+    $.ajax({
+        url: "https://api.forecast.io/forecast/" + forecastKey + "/" + lat + "," + long + "," + currentDate,
+        method: "GET"
+    }).done(function(data){
+        console.log(data);
+    })
+}
 
 //Using lat & long from geolocation, get the state (format: NY)
 var state;
@@ -34,14 +47,12 @@ function getCurrentWeather(pos){
 }
 
 function getHistory(){
-  console.log('in getHistory fn');
   $.ajax({
     // url: "http://api.wunderground.com/api/" + wuKey + "/history_" + currentDate + "/q/" + state + "/" + city + ".json",
     url: "http://api.wunderground.com/api/" + wuKey + "/almanac/q/NY/New_York.json",
     method: "GET"
 }).done(function(data){
     var avgHigh = data.almanac.temp_high.normal.F
-    console.log("average high: " + avgHigh);
     showHistory(data);
   });
 };
@@ -49,10 +60,13 @@ function getHistory(){
 function showHistory(data){
     var storySection = $('#story-section');
     storySection.append('<p> Average High Temperature: ' + data.almanac.temp_high.normal.F + ' ℉</p>');
-    storySection.append('<p> Record High Temperature: ' + data.almanac.temp_high.record.F + ' ℉</p>');
+    storySection.append('<p> A record high temperature of <strong>' + data.almanac.temp_high.record.F + ' ℉</strong> was recorded on this day in <strong>' + data.almanac.temp_high.recordyear + '</strong>.');
+    storySection.append('<p> Average Low Temperature: ' + data.almanac.temp_low.normal.F + ' ℉</p>');
+    storySection.append('<p> A record low temperature of <strong>' + data.almanac.temp_low.record.F + ' ℉</strong> was recorded on this day in <strong>' + data.almanac.temp_low.recordyear + '</strong>.');
+
 }
 
-function getApiKeys(){
+function getApiKeys(getTimeMachine){
   $.ajax({
     url: "/api",
     method: "GET"
@@ -63,4 +77,5 @@ function getApiKeys(){
     console.log(wuKey)
     // return "data";
   }).done(function(data){ getHistory() })
+  getTimeMachine();
 }
