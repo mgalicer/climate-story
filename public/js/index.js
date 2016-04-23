b0b662955038f404e67fcac9c00823aa051fab8a
Maggie Love <maggie.love1991@gmail.com>
2016-04-23 15:09:20 -0400

    adding route to retrieve keys from backend

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,8 @@
-'use strict'
+'use strict';
 
-var wuKey = process.env.WU_KEY
+var wuKey;
+
+var forecastKey;
 
 //Figure out how to pull the current date
 //Then convert the date to YYYYMMDD format.
@@ -12,36 +14,51 @@ var state;
 //using lat & long from geolocation, get the city (format: New_York)
 var city;
 
-
 var getHistory = function(){
   console.log('in getHistory fn');
-  $.ajax({
-    // url: "http://api.wunderground.com/api/" + wuKey + "/history_" + currentDate + "/q/" + state + "/" + city + ".json",
-    url: "http://api.wunderground.com/api/" + wuKey + "/history_20160423/q/NY/New_York.json",
-    method: "GET",
-    data: weatherHistory
-  }).done(function(weatherHistory){
-    console.log("data: " + weatherHistory);
-  });
+  // $.ajax({
+  //   // url: "http://api.wunderground.com/api/" + wuKey + "/history_" + currentDate + "/q/" + state + "/" + city + ".json",
+  //   url: "http://api.wunderground.com/api/" + wuKey + "/history_20160423/q/NY/New_York.json",
+  //   method: "GET",
+  //   data: weatherHistory
+  // }).done(function(weatherHistory){
+  //   console.log("data: " + weatherHistory);
+  // });
 };
 
 
 $( document ).ready(function() {
-  function(){
-    console.log('inside anonymous, jQueryless function');
-  }();
+  // function(){
+  //   console.log('inside anonymous, jQueryless function');
+  // }();
+
+    var getApiKeys = function(){
+      $.ajax({
+        url: "/api",
+        method: "GET"
+        // data: apiInfo
+      }).done(function(data){
+        console.log(data);
+        wuKey = data.wuKey;
+        forecastKey = data.forecastKey;
+        console.log(wuKey);
+      })
+    }
+
+    // console.log('WU:' + wuKey);
 
     navigator.geolocation.getCurrentPosition(function(position) {
       var position = [position.coords.latitude, position.coords.longitude];
     });
+    getApiKeys();
     getHistory();
 });
 
-});
+// });
 
 function getCurrentWeather(){
     $.ajax({
-      url : "http://api.wunderground.com/api/" + process.env.WU_KEY + "/geolookup/conditions/q/IA/" +  + ".json",
+      url : "http://api.wunderground.com/api/" + wuKey + "/geolookup/conditions/q/IA/" +  + ".json",
       dataType : "jsonp",
       success : function(parsed_json) {
       var location = parsed_json['location']['city'];
