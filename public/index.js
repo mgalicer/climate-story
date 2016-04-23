516814198a76ccc34575e6d40da5c6e95b2a8bb7
marigalicer <marigalicer@gmail.com>
2016-04-23 11:45:36 -0400

    fix file structure

--- public/index.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-$( document ).ready(function() {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      console.log(position.coords.latitude, position.coords.longitude);
-    });
-});
