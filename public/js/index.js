a4f2088a937f88ea2260d9b8ad023b71bab1062d
Maggie Love <maggie.love1991@gmail.com>
2016-04-23 13:12:39 -0400

    moved all js into index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,37 @@
 'use strict'
 
-var api = ('./api.js');
+var wuKey = process.env.WU_KEY
+
+//Figure out how to pull the current date
+//Then convert the date to YYYYMMDD format.
+var currentDate;
+
+//Using lat & long from geolocation, get the state (format: NY)
+var state;
+
+//using lat & long from geolocation, get the city (format: New_York)
+var city;
+
+
+var getHistory = function(){
+  console.log('in getHistory fn');
+  $.ajax({
+    // url: "http://api.wunderground.com/api/" + wuKey + "/history_" + currentDate + "/q/" + state + "/" + city + ".json",
+    url: "http://api.wunderground.com/api/" + wuKey + "/history_20160423/q/NY/New_York.json",
+    method: "GET",
+    data: weatherHistory
+  }).done(function(weatherHistory){
+    console.log("data: " + weatherHistory);
+  });
+};
 
 $( document ).ready(function() {
+  function(){
+    console.log('inside anonymous, jQueryless function');
+  }();
+
     navigator.geolocation.getCurrentPosition(function(position) {
       console.log(position.coords.latitude, position.coords.longitude);
     });
+    getHistory();
 });
