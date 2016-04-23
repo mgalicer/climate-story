e4b8e463d99dc9835adab653d05ad364ca7b36cf
Maggie Love <maggie.love1991@gmail.com>
2016-04-23 12:44:29 -0400

    adding initial history api ajax

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,7 @@
 'use strict'
+
+var api = ('./api.js');
+
 $( document ).ready(function() {
     navigator.geolocation.getCurrentPosition(function(position) {
       console.log(position.coords.latitude, position.coords.longitude);
